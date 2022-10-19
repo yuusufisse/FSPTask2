bca661f9fae78be81ec6d3ac8241f1f97f8b1ffc
Yusuf Farah <cyufa001@edu.xamk.fi>
2022-10-19 20:15:31 +0300

    Add project files.

---
+++ FSPTask2/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FSPTask2.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        public int TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+    }
+}
---
+++ FSPTask2/Models/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FSPTask2.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string TeacherName { get; set; }
+        public List<Course> Courses { get; set; }
+
+    }
+}
